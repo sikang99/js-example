3567adce581051e7093c2839a03c1c3775478d59
Stoney Kang <sikang99@gmail.com>
2019-09-08 00:28:21 +0900

    update information

---
+++ canvas/camera/README.md
@@ -0,0 +1,8 @@
+# canvas/camera
+
+### History
+- [Camera and Video Control with HTML5](https://davidwalsh.name/browser-camera) - [Demo](https://davidwalsh.name/demo/camera.php)
+
+- 2012/03/09 [I See You getUserMedia](http://tagsoup.github.io/blog/2012/03/09/i-see-you-getusermedia/)
+
+
---
+++ canvas/camera/test01/snapvideo.js
@@ -0,0 +1,49 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+// Grab elements, create settings, etc.
+var video = document.getElementById('video');
+
+function ToggleCamera(video) {
+    // check if camera is already on
+    if (video.srcObject) { 
+        video.srcObject = undefined;
+        return;
+    }
+    // Get access to the camera!
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        // Not adding `{ audio: true }` since we only want video now
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+            //video.src = window.URL.createObjectURL(stream);
+            video.srcObject = stream;
+            video.play();
+        });
+    }
+}
+
+document.getElementById("camera").addEventListener("click", function() {
+    if (video == undefined) return;
+    ToggleCamera(video);
+});
+
+function ToggleVideo(video) {
+    if (video.srcObject == undefined) return;
+    video.srcObject.getTracks().forEach(t => t.enabled = !t.enabled);
+}
+
+document.getElementById("toggle").addEventListener("click", function() {
+    ToggleVideo(video);
+});
+
+// Elements for taking the snapshot
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+// Trigger photo take
+document.getElementById("snap").addEventListener("click", function() {
+	context.drawImage(video, 0, 0, 320, 240);
+});
+
+//var log = msg => div.innerHTML += "<br>" + msg;
+
+console.log("ready to play");
+}, false);
